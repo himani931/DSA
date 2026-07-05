e06eeb6b694a1994b1c76bb5559374f883972d73
Himani Bhatnagar <himanibhatnagar9311@gmail.com>
2026-07-05 17:51:30 +0530

    Fibonacci number using dp with space optimization

--- dp_series/fibonacci.cpp
+++ dp_series/fibonacci.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+// top down approach
 int fib(int n, vector<int> &dp)
 {
   if (n == 0 || n == 1)
@@ -13,6 +14,37 @@ int fib(int n, vector<int> &dp)
   return dp[n];
 }
 
+// bottom approach
+int fibonacci(int n)
+{
+  vector<int> dp(n + 1);
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for (int i = 2; i <= n; i++)
+  {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+}
+
+// space optimization
+int fibon(int n)
+{
+  int prev1 = 1;
+  int prev2 = 0;
+
+  if (n == 0)
+    return prev2;
+
+  for (int i = 2; i <= n; i++)
+  {
+    int curr = prev1 + prev2;
+    prev2 = prev1;
+    prev1 = curr;
+  }
+  return prev1;
+}
 int main()
 {
   int n;
@@ -26,6 +58,10 @@ int main()
   }
 
   cout << fib(n, dp) << endl;
+
+  cout << fibonacci(n) << endl;
+
+  cout << fibon(n) << endl;
 
   return 0;
 }
