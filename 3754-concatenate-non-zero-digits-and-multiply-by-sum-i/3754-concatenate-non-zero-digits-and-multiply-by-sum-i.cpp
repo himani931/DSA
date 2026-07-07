f201e318f6df19f247ec3c6844d27fc583901283
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-07-07 14:04:56 +0530

    Time: 0 ms (100%), Space: 8.9 MB (89.09%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        long long x = 0;
+        long long sum = 0 ;
+        long long i = 1;
+        while(n > 0) {
+            int d = n % 10;
+            if(d != 0) {
+                x = d*i + x;
+                i *= 10;
+                sum += d;
+            }
+            n /= 10;
+        }
+        return x*sum;
+        
+    }
+};
