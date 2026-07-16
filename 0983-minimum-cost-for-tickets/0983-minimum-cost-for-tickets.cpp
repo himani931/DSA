82938da56a3506dcbf5a3920b04812d7dae7eef1
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-07-16 13:54:38 +0530

    Time: 0 ms (100%), Space: 13.2 MB (69.8%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -18,29 +18,48 @@ public:
 //     }
 
 
-    int solve(int n , vector<int>& days, vector<int>& costs , int index , vector<int> &dp) {
-        if(index >= n) return 0 ;
+    // int solve(int n , vector<int>& days, vector<int>& costs , int index , vector<int> &dp) {
+    //     if(index >= n) return 0 ;
 
-        if(dp[index] != -1) return dp[index];
+    //     if(dp[index] != -1) return dp[index];
 
-        int op1 = costs[0] + solve(n , days , costs , index+1 , dp);
+    //     int op1 = costs[0] + solve(n , days , costs , index+1 , dp);
+
+    //     int i ;
+    //     for(i = index ; i< n && days[i] < days[index]+ 7 ; i++);
+    //     int op2 = costs[1] + solve( n , days , costs , i , dp); 
+
+    //      for(i = index ; i< n && days[i] < days[index]+30 ; i++);
+    //      int op3 = costs[2] + solve(n , days , costs , i , dp);
+
+    //      dp[index] = min(op1 , min(op2 , op3)); 
+    //      return dp[index];
+        
+    // }
+
+
+ int solve(int n , vector<int>& days, vector<int>& costs) {
+        vector<int> dp(n+1 , INT_MAX);
+        dp[n] = 0;
+        for(int k = n-1 ; k>= 0 ; k--) {
+
+            int op1 = costs[0] + dp[k+1];
 
         int i ;
-        for(i = index ; i< n && days[i] < days[index]+ 7 ; i++);
-        int op2 = costs[1] + solve( n , days , costs , i , dp); 
+        for(i = k ; i< n && days[i] < days[k]+ 7 ; i++);
+        int op2 = costs[1] + dp[i]; 
 
-         for(i = index ; i< n && days[i] < days[index]+30 ; i++);
-         int op3 = costs[2] + solve(n , days , costs , i , dp);
+         for(i = k ; i< n && days[i] < days[k]+30 ; i++);
+         int op3 = costs[2] + dp[i];
 
-         dp[index] = min(op1 , min(op2 , op3)); 
-         return dp[index];
-        
+         dp[k] = min(op1 , min(op2 , op3)); 
+        }
+    return dp[0];
     }
-
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<int> dp(n+1 , -1);
-        return solve(n , days , costs , 0 , dp);
+        // vector<int> dp(n+1 , -1);
+        return solve(n , days , costs);
         
     }
 };
