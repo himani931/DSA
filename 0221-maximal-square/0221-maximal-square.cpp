c37c194876273bf52e35178347c47fd53c421de3
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-07-16 14:54:11 +0530

    Time: 4 ms (87.79%), Space: 31 MB (64.01%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//recursion
+    // recursion
 
     // int solve(vector<vector<char>>& matrix , int i , int j , int &maxi) {
     //     if(i >= matrix.size() || j >= matrix[0].size()) return 0;
@@ -16,29 +16,53 @@ public:
     //     }
     // }
 
-    //top down
+    // top down
 
-     int solve(vector<vector<char>>& matrix , int i , int j , int &maxi , vector<vector<int>> &dp) {
-        if(i >= matrix.size() || j >= matrix[0].size()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int right = solve(matrix , i , j+1 , maxi , dp);
-        int diagnol = solve(matrix , i+1 , j+1 , maxi , dp);
-        int down = solve(matrix, i+1 , j , maxi , dp);
-        if(matrix[i][j] == '1') {
-            dp[i][j] = 1 + min(right , min(diagnol , down));
-            maxi = max(dp[i][j] , maxi);
-            return dp[i][j];
-        } else {
-            return dp[i][j] = 0;
+    //  int solve(vector<vector<char>>& matrix , int i , int j , int &maxi ,
+    //  vector<vector<int>> &dp) {
+    //     if(i >= matrix.size() || j >= matrix[0].size()) return 0;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int right = solve(matrix , i , j+1 , maxi , dp);
+    //     int diagnol = solve(matrix , i+1 , j+1 , maxi , dp);
+    //     int down = solve(matrix, i+1 , j , maxi , dp);
+    //     if(matrix[i][j] == '1') {
+    //         dp[i][j] = 1 + min(right , min(diagnol , down));
+    //         maxi = max(dp[i][j] , maxi);
+    //         return dp[i][j];
+    //     } else {
+    //         return dp[i][j] = 0;
+    //     }
+    // }
+
+    // bottom up
+
+    int solve(vector<vector<char>>& matrix , int &maxi) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for (int i = n-1 ; i >= 0; i--) {
+            for (int j = m-1 ; j >= 0; j--) {
+                int right = dp[i][j+1];
+                int diagnol = dp[i + 1][j + 1];
+                int down = dp[i + 1][j];
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = 1 + min(right, min(diagnol, down));
+                    maxi = max(dp[i][j], maxi);
+                } else {
+                   dp[i][j] = 0;
+                }
+            }
         }
+        return dp[0][0];
     }
 
     int maximalSquare(vector<vector<char>>& matrix) {
         int maxi = 0;
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> dp( n, vector<int> (m , -1));
-        solve(matrix , 0 , 0 , maxi , dp);
-         return maxi * maxi;;
+        // int n = matrix.size();
+        // int m = matrix[0].size();
+        // vector<vector<int>> dp( n, vector<int> (m , -1));
+        // solve(matrix, 0, 0, maxi);
+        solve(matrix , maxi);
+        return maxi * maxi;
     }
 };
