cb529628bf89c4ce4059b2dc2f0397212b003549
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-07-20 11:13:45 +0530

    Time: 76 ms (55.45%), Space: 234.1 MB (65.83%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -40,33 +40,64 @@ public:
     //     }
     // }
 
-    // bottom yp
+    // bottom up
 
-    int solve(vector<int>& obstacles) {
-        int n = obstacles.size() -1;
-        vector<vector<int>> dp(4, vector<int>(n+1, INT_MAX));
+    // int solve(vector<int>& obstacles) {
+    //     int n = obstacles.size() -1;
+    //     vector<vector<int>> dp(4, vector<int>(n+1, INT_MAX));
 
-        dp[0][n] = 0;
-        dp[1][n] = 0;
-        dp[2][n] = 0;
-        dp[3][n] = 0;
+    //     dp[0][n] = 0;
+    //     dp[1][n] = 0;
+    //     dp[2][n] = 0;
+    //     dp[3][n] = 0;
+
+    //     for (int pos = n - 1; pos >= 0; pos--) {
+    //         for (int currlane = 1; currlane <= 3; currlane++) {
+    //             if (obstacles[pos + 1] != currlane) {
+    //                 dp[currlane][pos] = dp[currlane][pos + 1];
+    //             } else {
+    //                 int ans = INT_MAX;
+    //                 for (int i = 1; i <= 3; i++) {
+    //                     if (currlane != i && obstacles[pos] != i) {
+    //                         ans = min(ans, 1 + dp[i][pos+1]);
+    //                     }
+    //                 }
+    //                 dp[currlane][pos] = ans;
+    //             }
+    //         }
+    //     }
+    //     return min(dp[2][0] , min(1+dp[1][0] , 1+dp[3][0]));
+    // }
+    
+    //so
+
+       int solve(vector<int>& obstacles) {
+        int n = obstacles.size() -1;vector<vector<int>> dp(4, vector<int>(n+1, INT_MAX));
+        vector<int> curr(4 , INT_MAX);
+        vector<int> next (4 , INT_MAX);
+
+        next[0] = 0;
+        next[1] = 0;
+        next[2] = 0;
+        next[3] = 0;
 
         for (int pos = n - 1; pos >= 0; pos--) {
             for (int currlane = 1; currlane <= 3; currlane++) {
                 if (obstacles[pos + 1] != currlane) {
-                    dp[currlane][pos] = dp[currlane][pos + 1];
+                    curr[currlane] = next[currlane];
                 } else {
                     int ans = INT_MAX;
                     for (int i = 1; i <= 3; i++) {
                         if (currlane != i && obstacles[pos] != i) {
-                            ans = min(ans, 1 + dp[i][pos+1]);
+                            ans = min(ans, 1 + next[i]);
                         }
                     }
-                    dp[currlane][pos] = ans;
+                    curr[currlane] = ans;
                 }
             }
+            next = curr;
         }
-        return min(dp[2][0] , min(1+dp[1][0] , 1+dp[3][0]));
+        return min(next[2] , min(1+next[1] , 1+next[3]));
     }
 
         int minSideJumps(vector<int> & obstacles) {
