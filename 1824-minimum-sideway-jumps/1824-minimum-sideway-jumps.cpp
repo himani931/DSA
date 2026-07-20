d5ee12f98b4dd00ddd90909570e5a2d871a774b1
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-07-20 11:00:51 +0530

    Time: 44 ms (82.81%), Space: 234 MB (68.97%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,46 +1,76 @@
 class Solution {
 public:
-//recursion
-    // int solve(vector<int>& obstacles , int currlane , int pos){
-    //     int n = obstacles.size() - 1;
-    //     if(pos == n) return 0;
-    //     if(obstacles[pos+1] != currlane) {
-    //        return solve(obstacles , currlane , pos+1);
-    //     } else{
-    //         int ans = INT_MAX;
-    //         for(int i = 1 ; i<=3 ; i++) {
-    //             if(currlane != i && obstacles[pos] != i){
-    //             ans = min(ans , 1 + solve(obstacles , i , pos));
-    //             }
-    //         }
-    //          return ans;
+    // recursion
+    //  int solve(vector<int>& obstacles , int currlane , int pos){
+    //      int n = obstacles.size() - 1;
+    //      if(pos == n) return 0;
+    //      if(obstacles[pos+1] != currlane) {
+    //         return solve(obstacles , currlane , pos+1);
+    //      } else{
+    //          int ans = INT_MAX;
+    //          for(int i = 1 ; i<=3 ; i++) {
+    //              if(currlane != i && obstacles[pos] != i){
+    //              ans = min(ans , 1 + solve(obstacles , i , pos));
+    //              }
+    //          }
+    //           return ans;
 
     //     }
     // }
 
-    //top down
+    // top down
 
-     int solve(vector<int>& obstacles , int currlane , int pos , vector<vector<int>> &dp){
-        int n = obstacles.size() - 1;
-        if(pos == n) return 0;
-        if(dp[currlane][pos] != -1) return dp[currlane][pos];
-        if(obstacles[pos+1] != currlane) {
-           return solve(obstacles , currlane , pos+1 , dp);
-        } else{
-            int ans = INT_MAX;
-            for(int i = 1 ; i<=3 ; i++) {
-                if(currlane != i && obstacles[pos] != i){
-                ans = min(ans , 1 + solve(obstacles , i , pos , dp));
+    //  int solve(vector<int>& obstacles , int currlane , int pos ,
+    //  vector<vector<int>> &dp){
+    //     int n = obstacles.size() - 1;
+    //     if(pos == n) return 0;
+    //     if(dp[currlane][pos] != -1) return dp[currlane][pos];
+    //     if(obstacles[pos+1] != currlane) {
+    //        return solve(obstacles , currlane , pos+1 , dp);
+    //     } else{
+    //         int ans = INT_MAX;
+    //         for(int i = 1 ; i<=3 ; i++) {
+    //             if(currlane != i && obstacles[pos] != i){
+    //             ans = min(ans , 1 + solve(obstacles , i , pos , dp));
+    //             }
+    //         }
+    //         dp[currlane][pos] = ans;
+    //          return dp[currlane][pos];
+
+    //     }
+    // }
+
+    // bottom yp
+
+    int solve(vector<int>& obstacles) {
+        int n = obstacles.size() -1;
+        vector<vector<int>> dp(4, vector<int>(n+1, INT_MAX));
+
+        dp[0][n] = 0;
+        dp[1][n] = 0;
+        dp[2][n] = 0;
+        dp[3][n] = 0;
+
+        for (int pos = n - 1; pos >= 0; pos--) {
+            for (int currlane = 1; currlane <= 3; currlane++) {
+                if (obstacles[pos + 1] != currlane) {
+                    dp[currlane][pos] = dp[currlane][pos + 1];
+                } else {
+                    int ans = INT_MAX;
+                    for (int i = 1; i <= 3; i++) {
+                        if (currlane != i && obstacles[pos] != i) {
+                            ans = min(ans, 1 + dp[i][pos+1]);
+                        }
+                    }
+                    dp[currlane][pos] = ans;
                 }
             }
-            dp[currlane][pos] = ans;
-             return dp[currlane][pos];
-
         }
+        return min(dp[2][0] , min(1+dp[1][0] , 1+dp[3][0]));
     }
 
-    int minSideJumps(vector<int>& obstacles) {
-        vector<vector<int>> dp(4 , vector<int> (obstacles.size() , -1));
-        return solve(obstacles , 2 , 0 , dp);
-    }
-};
+        int minSideJumps(vector<int> & obstacles) {
+            // vector<vector<int>> dp(4 , vector<int> (obstacles.size() , -1));
+            return solve(obstacles);
+        }
+    };
