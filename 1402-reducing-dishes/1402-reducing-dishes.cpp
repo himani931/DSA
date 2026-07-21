8dd60d152576e2015b65e62a04c02a4f71daaffa
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-07-21 14:46:23 +0530

    Time: 8 ms (48.51%), Space: 12.4 MB (56.1%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -28,20 +28,40 @@ public:
     // }
 
     // bottom up
+    // int solve(vector<int>& satisfaction) {
+    //     int n = satisfaction.size();
+    //     vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    //     for (int index = n - 1; index >= 0; index--) {
+    //         for (int time = index; time >= 0; time--) {
+    //             int include = satisfaction[index] * (time + 1) +
+    //                           dp[index + 1][time + 1];
+    //             int exclude = 0 + dp[index + 1][time];
+    //             dp[index][time] = max(include, exclude);
+    //         }
+    //     }
+
+    //     return dp[0][0];
+    // }
+
+    //so
+
     int solve(vector<int>& satisfaction) {
         int n = satisfaction.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<int> curr(n+1 , 0);
+        vector<int> next(n+1 , 0);
 
         for (int index = n - 1; index >= 0; index--) {
             for (int time = index; time >= 0; time--) {
                 int include = satisfaction[index] * (time + 1) +
-                              dp[index + 1][time + 1];
-                int exclude = 0 + dp[index + 1][time];
-                dp[index][time] = max(include, exclude);
+                              next[time + 1];
+                int exclude = 0 + next[time];
+                curr[time] = max(include, exclude);
             }
+            next = curr;
         }
 
-        return dp[0][0];
+        return next[0];
     }
 
     int maxSatisfaction(vector<int>& satisfaction) {
