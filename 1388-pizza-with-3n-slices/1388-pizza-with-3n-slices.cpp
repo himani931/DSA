e9050d307217caa69c92e73adb12c152c25fc0eb
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-08-06 15:26:10 +0530

    Time: 4 ms (92.33%), Space: 12.4 MB (87.84%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -27,34 +27,72 @@ public:
 
     // bottom up
 
-    int solve(vector<int>& slices) {
+    // int solve(vector<int>& slices) {
+    //     int k = slices.size();
+    //     vector<vector<int>> dp1(k+2, vector<int>(k, 0));
+    //     vector<vector<int>> dp2(k+2, vector<int>(k, 0));
+
+    //     for (int i = k - 2; i >= 0; i--) {
+    //         for (int n = 1; n <= k / 3; n++) {
+    //             int take = slices[i] + dp1[i + 2][n - 1];
+    //             int notTake = 0 + dp1[i + 1][n];
+    //             dp1[i][n] = max(take, notTake);
+    //         }
+    //     }
+
+    //     int case1 = dp1[0][k/3];
+
+
+    //     for (int i = k - 1; i >= 1; i--) {
+    //         for (int n = 1; n <= k / 3; n++) {
+    //             int take = slices[i] + dp2[i + 2][n - 1];
+    //             int notTake = 0 + dp2[i + 1][n];
+    //             dp2[i][n] = max(take, notTake);
+    //         }
+    //     }
+
+    //     int case2 = dp2[1][k/3];
+
+    //     return max(case1 , case2);
+    // }
+
+    //so
+
+      int solve(vector<int>& slices) {
         int k = slices.size();
-        vector<vector<int>> dp1(k+2, vector<int>(k, 0));
-        vector<vector<int>> dp2(k+2, vector<int>(k, 0));
+        vector<int> prev1(k+2 , 0);
+        vector<int> curr1(k+2 , 0);
+        vector<int> next1(k+2 , 0);
+
+        vector<int> prev2(k+2 , 0);
+        vector<int> curr2(k+2 , 0);
+        vector<int> next2(k+2 , 0);
 
         for (int i = k - 2; i >= 0; i--) {
             for (int n = 1; n <= k / 3; n++) {
-                int take = slices[i] + dp1[i + 2][n - 1];
-                int notTake = 0 + dp1[i + 1][n];
-                dp1[i][n] = max(take, notTake);
+                int take = slices[i] + next1[n - 1];
+                int notTake = 0 + curr1[n];
+                prev1[n] = max(take, notTake);
             }
+            next1 = curr1;
+            curr1 = prev1;
         }
 
-        int case1 = dp1[0][k/3];
+        int case1 = curr1[k/3];
 
 
         for (int i = k - 1; i >= 1; i--) {
             for (int n = 1; n <= k / 3; n++) {
-                int take = slices[i] + dp2[i + 2][n - 1];
-                int notTake = 0 + dp2[i + 1][n];
-                dp2[i][n] = max(take, notTake);
+                int take = slices[i] + next2[n - 1];
+                int notTake = 0 + curr2[n];
+                prev2[n] = max(take, notTake);
             }
+            next2 = curr2;
+            curr2 = prev2;
         }
 
-        int case2 = dp2[1][k/3];
-
-
-
+        int case2 = curr2[k/3];
+        
         return max(case1 , case2);
     }
 
