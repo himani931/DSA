d2acd4c42637d29c7f0fa35de6e9730d969c1a88
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-08-12 12:33:35 +0530

    Time: 4 ms (81.81%), Space: 94.2 MB (87.43%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -51,9 +51,42 @@ public:
 
     // bottom up
 
+    // int solve(vector<int>& nums1, vector<int>& nums2) {
+
+    //     vector<vector<int>> dp(nums1.size()+1, vector<int>(2, 0));
+
+    //     for (int i = nums1.size() - 1; i >= 1; i--) {
+    //         for (int swapped = 1; swapped >= 0; swapped--) {
+
+    //             int ans = INT_MAX;
+    //             int prev1 = nums1[i - 1];
+    //             int prev2 = nums2[i - 1];
+
+    //             if (swapped) {
+    //                 swap(prev1, prev2);
+    //             }
+
+    //             if (nums1[i] > prev1 && nums2[i] > prev2) {
+    //                 ans = dp[i + 1][0];
+    //             }
+
+    //             if (nums1[i] > prev2 && nums2[i] > prev1) {
+    //                 ans = min(ans, 1 + dp[i + 1][1]);
+    //             }
+    //             dp[i][swapped] = ans;
+    //         }
+    //     }
+    //     return dp[1][0];
+    // }
+
+    //so
+
     int solve(vector<int>& nums1, vector<int>& nums2) {
 
-        vector<vector<int>> dp(nums1.size()+1, vector<int>(2, 0));
+        int swap = 0;
+        int noswap =0;
+        int currSwap = 0;
+        int currNoswap = 0;
 
         for (int i = nums1.size() - 1; i >= 1; i--) {
             for (int swapped = 1; swapped >= 0; swapped--) {
@@ -63,20 +96,28 @@ public:
                 int prev2 = nums2[i - 1];
 
                 if (swapped) {
-                    swap(prev1, prev2);
+                    int temp = prev2 ;
+                    prev2 = prev1 ;
+                    prev1 = temp;
                 }
 
                 if (nums1[i] > prev1 && nums2[i] > prev2) {
-                    ans = dp[i + 1][0];
+                    ans = noswap;
                 }
 
                 if (nums1[i] > prev2 && nums2[i] > prev1) {
-                    ans = min(ans, 1 + dp[i + 1][1]);
+                    ans = min(ans, 1 + swap);
                 }
-                dp[i][swapped] = ans;
+                if(swapped) {
+                    currSwap = ans;
+                } else{
+                    currNoswap = ans;
+                }
             }
+            swap = currSwap;
+            noswap = currNoswap;
         }
-        return dp[1][0];
+        return min(swap , noswap);
     }
 
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
