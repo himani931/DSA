79e27825dba418f7f97a0caeb29d12dfc53c3276
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-08-12 12:15:16 +0530

    Time: 119 ms (5.7%), Space: 138.6 MB (13.41%) - LeetHub

---
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    //Recursion
+
+    // int solve(vector<int> &nums1 , vector<int> &nums2 , int i , bool swapped) {
+    //     if(i == nums1.size()) return 0;
+    //     int ans = INT_MAX;
+    //     int prev1 = nums1[i -1];
+    //     int prev2 = nums2[i-1];
+
+    //     if(swapped) {
+    //     swap(prev1 , prev2);
+    //     }
+       
+    //    if(nums1[i] > prev1 && nums2[i] > prev2) {
+    //     ans = solve(nums1 , nums2 , i+1 , 0) ;
+    //    } 
+
+    //    if(nums1[i] > prev2 && nums2[i] > prev1) {
+    //     ans = min(ans , 1 + solve(nums1 , nums2 , i+1 , 1));
+    //    }
+    //    return ans;
+        
+    // }
+
+    //top down
+
+    int solve(vector<int> &nums1 , vector<int> &nums2 , int i , bool swapped , vector<vector<int>> &dp) {
+        if(i == nums1.size()) return 0;
+        int ans = INT_MAX;
+        int prev1 = nums1[i -1];
+        int prev2 = nums2[i-1];
+
+        if(dp[i][swapped] != -1) return dp[i][swapped];
+
+        if(swapped) {
+        swap(prev1 , prev2);
+        }
+       
+       if(nums1[i] > prev1 && nums2[i] > prev2) {
+        ans = solve(nums1 , nums2 , i+1 , 0 , dp) ;
+       } 
+
+       if(nums1[i] > prev2 && nums2[i] > prev1) {
+        ans = min(ans , 1 + solve(nums1 , nums2 , i+1 , 1 , dp));
+       }
+       return dp[i][swapped] = ans;
+        
+    }
+
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.begin() , -1);
+        nums2.insert(nums2.begin() , -1);
+
+        vector<vector<int>> dp(nums1.size() , vector<int>(2 , -1));
+        bool swapped = 0;
+        return solve(nums1 , nums2 , 1 , swapped , dp);
+    }
+};
