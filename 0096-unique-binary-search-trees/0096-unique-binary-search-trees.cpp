facee555d7867af280e460c9fd768306a1ba2cfa
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-08-18 19:01:40 +0530

    Time: 0 ms (100%), Space: 8.1 MB (42.71%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -16,21 +16,45 @@ public:
 
     //top down
 
-    int solve(int n , vector<int> &dp) {
-        if(n <= 1) return 1;
+    // int solve(int n , vector<int> &dp) {
+    //     if(n <= 1) return 1;
 
-        if(dp[n] != -1) return dp[n];
+    //     if(dp[n] != -1) return dp[n];
+
+    //     int ans = 0 ;
+
+    //     i = root node
+    //     for(int i = 1 ;  i<= n ; i++) {
+    //         ans += solve(i-1 , dp) * solve(n-i , dp);
+    //     }
+    //     return dp[n] = ans;
+    // }
+
+    //bottom up
+
+    int solve(int n) {
+
+        vector<int> dp(n+1 , 0);
+
+        dp[0] = 1;
+        dp[1] = 1;
 
         int ans = 0 ;
 
-        for(int i = 1 ;  i<= n ; i++) {
-            ans += solve(i-1 , dp) * solve(n-i , dp);
+        // i = number of nodes
+        for(int i = 2 ;  i<= n ; i++) {
+
+            // j = root node
+
+            for(int j = 1 ; j <= i ; j++) {
+                dp[i] += dp[j-1] * dp[i-j];
+            }
         }
-        return dp[n] = ans;
+        return dp[n];
     }
 
     int numTrees(int n) {
-        vector<int> dp(n+1 , -1);
-        return solve(n , dp);
+        // vector<int> dp(n+1 , -1);
+        return solve(n);
     }
 };
