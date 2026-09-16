b463dc76b546befba04bdf07036a91dea574820d
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-09-16 15:18:47 +0530

    Time: 3 ms (61.43%), Space: 10.1 MB (77.64%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,52 +1,102 @@
 class Solution {
 public:
 
-    bool isSafe(int i , int j , vector<string> &board , int n) {
-        int x = i ; //row
-        int y = j; //col
-        //check for row
-        while(y>=0) {
-            if(board[x][y] == 'Q') {
-                return false;
-            }
+    // bool isSafe(int i , int j , vector<string> &board , int n) {
+    //     int x = i ; //row
+    //     int y = j; //col
+    //     //check for row
+    //     while(y>=0) {
+    //         if(board[x][y] == 'Q') {
+    //             return false;
+    //         }
+    //         y--;
+    //     }
+    // //check for diagonal
+    // x=i;
+    // y=j;
+    // //upper
+    // while(x>=0 && y>=0) {
+    //      if(board[x][y] == 'Q') {
+    //             return false;
+    //         }
+    //         y--;
+    //         x--;
+    // }
+    // //lower
+    // x=i;
+    // y=j;
+    // while(x<n && y>=0) {
+    //      if(board[x][y] == 'Q') {
+    //             return false;
+    //         }
+    //         y--;
+    //         x++;
+    // }
+    // return true;
+    // }
+
+    // void solve(int col , vector<vector<string>> &ans , vector<string> &board , int n) {
+    //     if(col == n) {
+    //        ans.push_back(board);
+    //         return;
+    //     }
+
+    //     for(int i = 0 ; i < n ; i++) {
+    //         if(isSafe(i , col , board , n)) {
+    //             board[i][col] = 'Q';
+    //             solve(col+1 , ans , board , n);
+    //             //backtrack
+    //             board[i][col] = '.';
+    //         }
+    //     }
+    // }
+
+    bool isSafe(int row , int col , int n , vector<string> &board) {
+        //horizontal
+        for(int i = 0 ; i < n ; i++) {
+            if(board[i][col] == 'Q') return false;
+        }
+
+        //vertical
+        for(int j = 0 ; j < n ; j++) {
+            if(board[row][j] == 'Q') return false;
+        }
+
+        //diagonal
+        //left
+        int x = row ;
+        int y = col;
+
+        while(x >= 0 && y >= 0) {
+            if(board[x][y] == 'Q') return false;
+            x--;
             y--;
         }
-    //check for diagonal
-    x=i;
-    y=j;
-    //upper
-    while(x>=0 && y>=0) {
-         if(board[x][y] == 'Q') {
-                return false;
-            }
-            y--;
+
+        //right
+        x = row;
+        y = col;
+
+        while(x >= 0 && y < n) {
+            if(board[x][y] == 'Q') return false;
             x--;
-    }
-    //lower
-    x=i;
-    y=j;
-    while(x<n && y>=0) {
-         if(board[x][y] == 'Q') {
-                return false;
-            }
-            y--;
-            x++;
-    }
-    return true;
+            y++;
+        }
+        return true;
     }
 
-    void solve(int col , vector<vector<string>> &ans , vector<string> &board , int n) {
-        if(col == n) {
-           ans.push_back(board);
+    void solve(int row , vector<vector<string>> &ans , vector<string> &board , int n) {
+        if(row >= n) {
+            ans.push_back(board);
             return;
         }
 
+
         for(int i = 0 ; i < n ; i++) {
-            if(isSafe(i , col , board , n)) {
-                board[i][col] = 'Q';
-                solve(col+1 , ans , board , n);
-                //backtrack
-                board[i][col] = '.';
+            if(isSafe(row , i , n , board)) {
+                board[row][i] = 'Q';
+                solve(row+1 , ans , board , n);
+                board[row][i] = '.';
             }
         }
     }
