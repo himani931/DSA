2037316a8678430411811421abf0dd77efc59652
HIMANI BHATNAGAR <himanibhatnagar9311@gmail.com>
2026-09-23 11:35:00 +0530

    Time: 24 ms (49.41%), Space: 23.4 MB (65.99%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,13 +11,24 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == NULL) return NULL;
-        if(root->val > p->val && root->val > q->val) {
-            return lowestCommonAncestor(root->left , p , q);
-        } else if (root->val < p->val && root->val < q->val) {
-            return lowestCommonAncestor(root->right , p , q);
-        } else {
-            return root;
-        }
+        // if(root == NULL) return NULL;
+        // if(root->val > p->val && root->val > q->val) {
+        //     return lowestCommonAncestor(root->left , p , q);
+        // } else if (root->val < p->val && root->val < q->val) {
+        //     return lowestCommonAncestor(root->right , p , q);
+        // } else {
+        //     return root;
+        // }
+
+
+         if(root == NULL) return NULL;
+        if(root == p || root == q) return root;
+
+     
+        TreeNode* leftLca = lowestCommonAncestor(root->left , p , q);
+        TreeNode* rightLca = lowestCommonAncestor(root->right , p ,q);
+        if(leftLca && rightLca) return root;
+        else if(leftLca) return leftLca;
+        else return rightLca;
     }
 };
